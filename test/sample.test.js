a300e1ae67da7c28a9b7efcc703cbe59ae6ec5e4
zhangzhang1 <zhangzhang1@fitbiz.com>
2018-03-09 18:09:45 +0800

    ·

--- test/sample.test.js
+++
@@ -1,9 +0,0 @@
-import assert from 'power-assert';
-
-describe('Sample test cases', e => {
-
-    it('hello test', async () => {
-        const name = 'test';
-        assert(name === 'test');
-    });
-});
